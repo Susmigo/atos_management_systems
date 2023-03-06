f9e08c8b7e1bc0cf1a77eedcf469b11405723c82
Govardhan Gautam <vardhank9@gmail.com>
2023-03-06 23:16:40 +0530

    added userregistrationDto

--- src/main/java/com/atos/management/systems/atos_management_systems/dto/UserRegistrationDto.java
+++ src/main/java/com/atos/management/systems/atos_management_systems/dto/UserRegistrationDto.java
@@ -2,20 +2,49 @@ package com.atos.management.systems.atos_management_systems.dto;
 
 public class UserRegistrationDto {
 
-    public Object getFirstName() {
-        return null;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public UserRegistrationDto(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
-        return null;
+        return lastName;
     }
 
-    public CharSequence getPassword() {
-        return null;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
-        return null;
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
 }
